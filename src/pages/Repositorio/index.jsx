49d5bfeb5059f14300944156e9c760dc9a5f6033
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-09-16 16:45:57 -0300

    aula 26 completa

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
-import { Container } from "./style";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./style";
+import {FaArrowLeft} from 'react-icons/fa'
 import api from '../../services/api'
 
 
@@ -28,6 +29,7 @@ export default function Repositorio({match}){
 
       setRepositorio(repositorioData.data)
       setIssues(issuesData.data)
+      console.log(issuesData.data)
       setLoading(false)
     }
 
@@ -35,9 +37,52 @@ export default function Repositorio({match}){
 
   }, [match.params.repositorio])
 
+  if(loading){
+    return(
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    )
+  }
+
   return (
     <Container>
-      
+
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+
+      <Owner>
+        <img src={repositorio.owner.avatar_url} 
+        alt={repositorio.owner.login} />
+        <h1>{repositorio.name}</h1>
+        <p>{repositorio.description}</p>
+      </Owner>
+
+      <IssuesList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>
+                    {label.name}
+                  </span>
+                ))}
+
+              </strong>
+
+              <p>{issue.user.login}</p>
+
+            
+
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   )
 }
